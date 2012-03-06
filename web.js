f0d32e68238cb1dca90cacd928b5963d36f58521
Valentina Camacho <valen.camacho@gmail.com>
2012-03-06 09:43:04 -0500

    change the app.get and instead of urlslug is newEnty.booknumber

--- web.js
+++ web.js
@@ -74,7 +74,7 @@ app.get('/', function(request, response) {
 
 
 // Display a single blog post
-app.get('/entry/:urlslug',function(request, response){
+app.get('/book/:newEntry.bookNumber',function(request, response){
     
     // Get the request blog post by urlslug
     Book.findOne({urlslug:request.params.urlslug},function(err,post){
